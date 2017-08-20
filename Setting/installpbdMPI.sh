00dff5ff8a5557136e3d619ff30f772e77fcf3c8
EnigmaSong <adrenalin789@gmail.com>
2017-08-20 02:56:11 -0600

    installpbdMPI.sh

---
+++ Setting/installpbdMPI.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+module purge 
+module load R
+module load openmpi/1.10.2
+
+wget https://cran.r-project.org/src/contrib/rlecuyer_0.3-4.tar.gz
+
+R CMD INSTALL rlecuyer_0.3-4.tar.gz
+
+wget https://cran.r-project.org/src/contrib/pbdMPI_0.3-3.tar.gz
+
+R CMD INSTALL pbdMPI_0.3-3.tar.gz --configure-args=" \
+  --with-mpi-type=OPENMPI \
+  --with-mpi-include=$CURC_OPENMPI_INC \
+  --with-mpi-libpath=$CURC_OPENMPI_LIB" \
+  --library=/projects/$USER/R/library/ --no-test-load
