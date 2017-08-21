6b77d346c5f4f6909b1b4a76b69e379e2f007815
Youngseok Song <songy@stat.colostate.edu>
2017-08-21 14:28:16 -0600

    Update installpbdMPI.sh

--- Setting/installpbdMPI.sh
+++ Setting/installpbdMPI.sh
@@ -13,4 +13,4 @@ R CMD INSTALL pbdMPI_0.3-3.tar.gz --configure-args=" \
   --with-mpi-type=OPENMPI \
   --with-mpi-include=$CURC_OPENMPI_INC \
   --with-mpi-libpath=$CURC_OPENMPI_LIB" \
-  --library=/projects/$USER/R/library/ --no-test-load
+  --library=$R_LIBS --no-test-load
