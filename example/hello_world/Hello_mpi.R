44027c4f1a8ea24d62a1a050142b30d25d26853f
Youngseok Song <songy@stat.colostate.edu>
2017-08-21 23:11:36 -0600

    Create Hello_mpi.R

---
+++ example/hello_world/Hello_mpi.R
@@ -0,0 +1,14 @@
+############
+# Source : http://hpcf-files.umbc.edu/code/Rmpi-hello-spmd/hello.R
+library(Rmpi)
+
+id <- mpi.comm.rank(comm = 0)
+np <- mpi.comm.size(comm = 0)
+hostname <- mpi.get.processor.name()
+
+msg <- sprintf("Hello world from process %03d of %03d, on host %s\n",
+        id, np, hostname)
+cat(msg)
+
+mpi.barrier(comm = 0)
+mpi.finalize()
